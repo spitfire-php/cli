449d2d2727f436c7c7931f0e9c1a2a2754d60ab0
César de la Cal Bretschneider <cesar@magic3w.com>
2021-06-10 12:13:15 +0200

    Adds test for the flag consumer
    Test Plan: `composer test`
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D822

---
+++ tests/consumer/FlagConsumerTest.php
@@ -0,0 +1,46 @@
+<?php 
+
+/* 
+ * Copyright (C) 2021 César de la Cal Bretschneider <cesar@magic3w.com>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+use PHPUnit\Framework\TestCase;
+use spitfire\cli\arguments\ArgumentBuffer;
+use spitfire\cli\arguments\CLIParameters;
+
+class FlagConsumerTest extends TestCase
+{
+	
+	public function testConsume()
+	{
+		$schema = [
+			'key' => '-v',
+			'description' => 'Sets the verbosity level',
+			'multiple' => true
+		];
+		
+		$buffer = new ArgumentBuffer(['-v', '-v']);
+		$params = new CLIParameters();
+		
+		$consumer = new \spitfire\cli\consumer\FlagConsumer($schema);
+		$consumer->consume($buffer, $params);
+		
+		$this->assertEquals(2, $params->get('v'));
+		
+	}
+}
